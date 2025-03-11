14925934d34bc97d409ab6334bcc0f56087e752d
Marijn Bankers <contact@marijnbankers.nl>
2025-03-11 13:51:51 +0100

    Link element added

---
+++ packages/storybook/src/components/Link/_Link.tsx
@@ -0,0 +1,18 @@
+export const argTypes = {
+  href: {
+    name: 'href',
+    type: { name: 'string', required: false },
+  },
+  target: {
+    name: 'target',
+    type: { name: 'string', required: false },
+  },
+  rel: {
+    name: 'rel',
+    type: { name: 'string', required: false },
+  },
+  placeholder: {
+    name: 'placeholder',
+    type: { name: 'boolean', required: false },
+  },
+};
