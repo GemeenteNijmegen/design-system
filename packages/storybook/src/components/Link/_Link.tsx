39852d6fd3ae52d0c2c511443ccc7ae00eb51aef
Michel Verhoeven <michel@woweb.nl>
2025-03-14 22:32:18 +0100

    temporary excl argTypes for html link implementation

--- packages/storybook/src/components/Link/_Link.tsx
+++ packages/storybook/src/components/Link/_Link.tsx
@@ -1,18 +1,19 @@
 export const argTypes = {
-  href: {
-    name: 'href',
-    type: { name: 'string', required: false },
-  },
-  target: {
-    name: 'target',
-    type: { name: 'string', required: false },
-  },
-  rel: {
-    name: 'rel',
-    type: { name: 'string', required: false },
-  },
-  placeholder: {
-    name: 'placeholder',
-    type: { name: 'boolean', required: false },
-  },
+  // @todo: Fix argtypes https://github.com/WowebNL/design-system-nijmegen/actions/runs/13865525247/job/38803719569
+  // href: {
+  //   name: 'href',
+  //   type: { name: 'string', required: false },
+  // },
+  // target: {
+  //   name: 'target',
+  //   type: { name: 'string', required: false },
+  // },
+  // rel: {
+  //   name: 'rel',
+  //   type: { name: 'string', required: false },
+  // },
+  // placeholder: {
+  //   name: 'placeholder',
+  //   type: { name: 'boolean', required: false },
+  // },
 };
