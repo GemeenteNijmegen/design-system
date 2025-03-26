43c7aea2da4bfccc51d0627dd2b8444a18a9c5ae
Michel Verhoeven <michel@woweb.nl>
2025-03-26 20:44:39 +0100

    wip: header

--- packages/storybook/src/components/Header/Html/Header.stories.tsx
+++ packages/storybook/src/components/Header/Html/Header.stories.tsx
@@ -43,10 +43,10 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 export const Default: Story = {
-  name: 'Link',
-  args: {
-    href: 'https://example.com',
-    title: 'Link',
-    children: 'Link',
-  },
+  name: 'Header',
+  //   args: {
+  //     href: 'https://example.com',
+  //     title: 'Link',
+  //     children: 'Link',
+  //   },
 };
