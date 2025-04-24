b011b063e16ef4f51e1e49398e831b23b5c7c13b
Marijn Bankers <contact@marijnbankers.nl>
2025-04-24 14:27:20 +0200

    Ordered list component added

---
+++ packages/storybook/src/components/Ordered list/React/OrderedList.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { OrderedList, OrderedListItem } from '@utrecht/component-library-react/dist/css-module';
+import { argTypes } from '../_OrderedList';
+
+const meta = {
+  title: 'Components/Ordered list/React Implementation',
+  id: 'react-ordered-list',
+  component: OrderedList,
+  argTypes: argTypes,
+  args: {
+    children: '',
+  },
+  parameters: {
+    status: {
+      type: 'BETA',
+    },
+  },
+} satisfies Meta<typeof OrderedList>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DefaultOrderedList: Story = {
+  args: {
+    children: [
+      <OrderedListItem key="li-1">List item 1</OrderedListItem>,
+      <OrderedListItem key="li-2">List item 2</OrderedListItem>,
+      <OrderedListItem key="li-3">List item 3</OrderedListItem>,
+    ],
+  },
+};
+
+export const NestedOrderedList: Story = {
+  args: {
+    children: [
+      <OrderedListItem key="li-4">List item 1</OrderedListItem>,
+      <OrderedListItem key="li-5">
+        List item 2
+        <OrderedList>
+          <OrderedListItem>Nested List item 1</OrderedListItem>
+          <OrderedListItem>Nested List item 2</OrderedListItem>
+        </OrderedList>
+      </OrderedListItem>,
+      <OrderedListItem key="li-6">List item 3</OrderedListItem>,
+    ],
+  },
+};
