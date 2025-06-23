9f30a35d584e81b699fed056fa082879c7fc3919
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-06-23 07:26:50 +0000

    Deploying to gh-pages from @ GemeenteNijmegen/design-system@0a5d7af5c6ad42642e3f0b602984fe8a3ad518c8 🚀

---
+++ assets/LinkList.stories-3rcQofgL.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{a as i}from"./index-I-h-doQp.js";import{I as a}from"./IconChevronRight-DyYvLEvC.js";const o={},c={title:"Components/Link list/React Implementation",id:"react-link-list",component:i,argTypes:o,args:{},parameters:{status:{type:"BETA"}}},n={name:"Link list",args:{icon:()=>e.jsx("span",{className:"utrecht-icon",children:e.jsx(a,{})}),links:[{children:"Link 1",href:"#"},{children:"Link 2",href:"#"},{children:"Link 3",href:"#"}]}};var r,t,s;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Link list',
+  args: {
+    icon: () => <span className="utrecht-icon">
+        <IconChevronRight />
+      </span>,
+    links: [{
+      children: 'Link 1',
+      href: '#'
+    }, {
+      children: 'Link 2',
+      href: '#'
+    }, {
+      children: 'Link 3',
+      href: '#'
+    }]
+  }
+}`,...(s=(t=n.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const l=["Default"],d=Object.freeze(Object.defineProperty({__proto__:null,Default:n,__namedExportsOrder:l,default:c},Symbol.toStringTag,{value:"Module"}));export{d as L};
