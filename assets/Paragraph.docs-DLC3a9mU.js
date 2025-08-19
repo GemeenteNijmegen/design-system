4e602f176a97bec5dc611c2deba20bfb297f48df
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-08-19 10:26:21 +0000

    Deploying to gh-pages from @ GemeenteNijmegen/design-system@4303fbf4d724a3b55636a9b1b3cf18e1b328dfbd 🚀

--- assets/Paragraph.docs-DLC3a9mU.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as e,P as i,b as s}from"./chunk-HLWAVYOI-CBsb4Ktb.js";import{P as p}from"./Paragraph.stories-7XapLCZw.js";import{useMDXComponents as m}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DyutJSzZ.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-ToFVweKJ.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index.esm--jZWF5K0.js";import"./clsx-DxZ0A2-Z.js";import"./server.browser-rAMDWieH.js";function r(n){const o=Object.assign({h1:"h1",h2:"h2"},m(),n.components);return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(e,{of:p}),`
-`,t.jsx(o.h1,{id:"paragraph-component-implementation-in-htmlcss",children:"Paragraph Component implementation in HTML/CSS"}),`
-`,t.jsx(o.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
-`,t.jsx(i,{}),`
-`,t.jsx(s,{})]})}function X(n={}){const{wrapper:o}=Object.assign({},m(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(r,n)})):r(n)}export{X as default};
