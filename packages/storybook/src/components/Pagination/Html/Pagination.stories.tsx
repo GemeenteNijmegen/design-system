ec8518ad90046206381d95850a7d6a4ada50bba2
Marijn Bankers <contact@marijnbankers.nl>
2025-10-15 12:08:59 +0200

    Pagination: Fixed documentation and responsive issue

--- packages/storybook/src/components/Pagination/Html/Pagination.stories.tsx
+++ packages/storybook/src/components/Pagination/Html/Pagination.stories.tsx
@@ -52,32 +52,21 @@ export const Default: Story = {
       href: '',
       disabled: true,
     },
-    links: [
-      {
-        href: './1',
-        index: 1,
-        title: 'Resultaat 1 tot 10',
-      },
-      {
-        href: './2',
-        index: 2,
-        title: 'Resultaat 11 tot 20',
-      },
-      {
-        href: './3',
-        index: 3,
-        title: 'Resultaat 21 tot 30',
-      },
-      {
-        href: './4',
-        index: 4,
-        title: 'Resultaat 31 tot 40',
-      },
-      {
-        href: './5',
-        index: 5,
-        title: 'Resultaat 41 tot 50',
-      },
-    ],
+    ellipses: false,
+  },
+};
+
+export const Ellipses: Story = {
+  name: 'Pagination with ellipses',
+  args: {
+    next: {
+      href: '',
+      disabled: false,
+    },
+    prev: {
+      href: '',
+      disabled: true,
+    },
+    ellipses: true,
   },
 };
