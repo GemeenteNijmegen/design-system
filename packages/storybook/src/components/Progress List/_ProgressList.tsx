a19f8439b920399e22f66e1bed4781cbdd687b72
Marijn Bankers <contact@marijnbankers.nl>
2026-05-20 10:00:42 +0200

    Progress-list WIP

---
+++ packages/storybook/src/components/Progress List/_ProgressList.tsx
@@ -0,0 +1,18 @@
+import '@gemeentenijmegen/components-css';
+
+export const argTypes = {};
+
+export const ProgressListStory = () => {
+  return (
+    <ol className="nijmegen-progress-list">
+      <li className="nijmegen-progress-list__item">
+        <div className="nijmegen-progress-list__header">
+          <h5 className="utrecht-heading-5">Heading</h5>
+        </div>
+        <div className="nijmegen-progress-list__body">
+          <p className="utrecht-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+        </div>
+      </li>
+    </ol>
+  );
+};
