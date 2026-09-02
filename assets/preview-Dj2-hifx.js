a6779fa3f4528731d5f3e086a09c4fa5d4c78048
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2026-09-02 11:24:17 +0000

    Deploying to gh-pages from @ GemeenteNijmegen/design-system@61827a09dca4087899b60256e2cec36a914c2319 🚀

---
+++ assets/preview-Dj2-hifx.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-baCLRd36.js","./chunk-HLWAVYOI-jAfN5g35.js","./iframe-B_451-QI.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-CnWNB3mc.js","./extends-CCbyfPlC.js","./_getPrototype-nBa_wnor.js","./index-ltOY3w3e.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as e}from"./iframe-B_451-QI.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-baCLRd36.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
